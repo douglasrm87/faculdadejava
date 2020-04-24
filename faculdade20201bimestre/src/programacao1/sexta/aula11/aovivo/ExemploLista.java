2643f49ae9209ba851c3c1198070bf027ae0c64b
Your Name <you@example.com>
2020-04-24 20:16:42 -0300

    lista

--- faculdade20201bimestre/src/programacao1/sexta/aula11/aovivo/ExemploLista.java
+++ faculdade20201bimestre/src/programacao1/sexta/aula11/aovivo/ExemploLista.java
@@ -14,10 +14,22 @@ public class ExemploLista {
 		lista.add("João");
 		lista.add("Pedro");
 		lista.add("Eu");
+		lista.add(1);
+		lista.add(22);
+		lista.add(134);
 		for (int i = 0; i < lista.size(); i++) {
 			System.out.println(lista.get(i));
 		}
 		
+		List lista2 = new ArrayList();
+		lista2.add(90);
+		lista2.add(30);
+		lista2.add(90);
+		double total = 0.0;
+		for (int i = 0; i < lista.size(); i++) {
+			total = total + (double) lista2.get(i);
+		}
+		System.out.println("Media: " + total/lista2.size());
 		
 		
 		
