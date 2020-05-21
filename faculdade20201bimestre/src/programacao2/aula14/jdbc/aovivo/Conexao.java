77ed758845c2d86b8c889109e0b5c124240bd927
Your Name <you@example.com>
2020-05-21 20:26:04 -0300

    sqlexception

--- faculdade20201bimestre/src/programacao2/aula14/jdbc/aovivo/Conexao.java
+++ faculdade20201bimestre/src/programacao2/aula14/jdbc/aovivo/Conexao.java
@@ -1,9 +1,14 @@
 package programacao2.aula14.jdbc.aovivo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 	public static void main(String[] args) {
 		new Conexao().processar();
 	}
+
 	private void processar() {
 		System.out.println("Ola minha primeira conexão.");
 		try {
@@ -12,8 +17,25 @@ public class Conexao {
 			// para postgree
 			Class.forName("org.postgresql.Driver");
 			System.out.println("Apos Class.forname().");
+			
+//			String url = "jdbc:mysql://localhost:3306/seguro?createDatabaseIfNotExist=true";
+			String url = "jdbc:postgresql://localhost:5432/faculdade";
+			Connection con = DriverManager.getConnection(url, "postgres", "1234567");
+			System.out.println("Apos obter conexao.");
+			con.close();
+			
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Diver inválido.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			if (e instanceof SQLException) {
+				System.out.println("MSG:" + e.getErrorCode());
+				System.out.println("MSG:" + e.getMessage());
+				System.out.println("MSG:" + e.getSQLState());
+				
+			}
+			System.out.println("local do banco incorreto, ou nome do banco incorreto, ou porta incorreta, ou usuario invalido ou senha invalida");
 			e.printStackTrace();
 		}
 
