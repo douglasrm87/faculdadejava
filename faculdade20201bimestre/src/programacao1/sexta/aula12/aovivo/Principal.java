5960e48510778a6241f8d2f2e18b3d6a1fea0d27
Your Name <you@example.com>
2020-05-29 20:34:25 -0300

    ajustes

--- faculdade20201bimestre/src/programacao1/sexta/aula12/aovivo/Principal.java
+++ faculdade20201bimestre/src/programacao1/sexta/aula12/aovivo/Principal.java
@@ -7,26 +7,19 @@ public class Principal {
 	public static void main(String[] args) {
 		new Principal().processar();
 	}
+
 	private void processar() {
 		List<Sapato> listaSapato = new ArrayList<>();
-		
 		Sapato s1 = new Sapato("salto alto", 34);
-		listaSapato.add(s1);
-		
+		listaSapato.add(s1); // adicionar em memoria
 		s1 = new Sapato("salto baixo", 33);
-		listaSapato.add(s1);
-		
+		listaSapato.add(s1);// adicionar em memoria
 		s1 = new Sapato("tennis", 37);
-		listaSapato.add(s1);
-		
+		listaSapato.add(s1);// adicionar em memoria
 		s1 = new Sapato("chinello", 39);
 		listaSapato.add(s1);
-		
 		for (Sapato sapato : listaSapato) {
 			System.out.println(sapato);
 		}
-		
-		
 	}
-
 }
